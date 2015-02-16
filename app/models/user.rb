f005e70a71f0e2dcfd881f8dd5753b9865fc4853
Ian <iantskon@gmail.com>
2015-02-15 20:00:49 -0800

    Setup rails backend for auth

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	has_secure_password
+
+	validates :username, uniqueness: true, presence: true
+	
+	def self.authenticate username, password
+    User.find_by_username(username).try(:authenticate, password)
+  end
+end
