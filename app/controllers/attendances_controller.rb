acd4c4d8c41fb5c684a8139a00f6a7c0941f49d8
Ian <iantskon@gmail.com>
2015-02-17 11:54:21 -0800

    Modify attendance params

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -54,6 +54,6 @@ class AttendancesController < ApplicationController
     end
 
     def attendance_params
-      params.require(:attendance).permit(:name)
+      params.require(:attendance).permit(:name, :user_id)
     end
 end
