801f08ecdd7c603eba9d0fef84064f06127f28d4
Ian <iantskon@gmail.com>
2015-03-15 20:48:16 -0700

    Remove course_id from attendances

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314230030) do
+ActiveRecord::Schema.define(version: 20150316034652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20150314230030) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.integer  "course_id"
     t.string   "secret"
   end
 
