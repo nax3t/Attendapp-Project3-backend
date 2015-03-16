f257bc3ede3682e6251c396603c2ae71d1d55dcb
Ian <iantskon@gmail.com>
2015-03-15 20:55:33 -0700

    Remove user_id from attendances

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316034652) do
+ActiveRecord::Schema.define(version: 20150316035435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20150316034652) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
     t.string   "secret"
   end
 
